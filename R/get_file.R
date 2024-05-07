667a8535ef42c0d696a9e5b10878a006e8a81bec
AlexWeinreb <alexis.weinreb@yale.edu>
2024-05-07 13:54:22 -0400

    Download with https (not ftp)

--- R/get_file.R
+++ R/get_file.R
@@ -1,31 +1,48 @@
+get_url_root <- function(protocol = c("https", "ftp")){
+  protocol <- match.arg(protocol)
+
+  if(protocol == "ftp"){
+    root <- "ftp://ftp.wormbase.org/pub/wormbase/releases/"
+  } else {
+    root <- "https://downloads.wormbase.org/releases/"
+  }
+
+  root
+}
+
+
+
 #' Get FTP path and filename of the geneIDs table
 #'
 #' This internal function gets the Wormbase FTP location of the filename
 #' of the geneIDs table for a given Wormbase release.
 #'
 #' @param WS Wormbase release number.
+#' @param protocol use https or ftp
 #'
 #' @return a vector of length 2 giving the FTP path and the filename.
 #'
-get_filename_geneID <- function(WS){
+get_filename_geneID <- function(WS, protocol = "https"){
+
+  base_url <- get_url_root(protocol)
+
   if(WS >= 237){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/PRJNA13758/annotation/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/PRJNA13758/annotation/"),
               filename=paste0("c_elegans.PRJNA13758.WS",WS,".geneIDs.txt.gz"))
 
   } else if(WS >= 226){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/annotation/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/annotation/"),
               filename=paste0("c_elegans.WS",WS,".geneIDs.txt.gz"))
 
   } else if(WS >= 197){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/annotation/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/annotation/"),
               filename=paste0("geneIDs.WS",WS,".gz"))
 
   } else if(WS %in% seq(130, 190, 10)){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/"),
+    path <- c(path=paste0(base_url, "WS", WS,"/"),
               filename=paste0("geneIDs.WS",WS,".gz"))
 
   } else if(WS < 197){
@@ -43,14 +60,17 @@ get_filename_geneID <- function(WS){
 #' of the GTF file for a given Wormbase release.
 #'
 #' @param WS Wormbase release number.
+#' @param protocol use https or ftp
 #'
 #' @return a vector of length 2 giving the FTP path and the filename.
 #'
-get_filename_gtf <- function(WS){
+get_filename_gtf <- function(WS, protocol = "https"){
+
+  base_url <- get_url_root(protocol)
 
   if(WS >= 253){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/PRJNA13758/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/PRJNA13758/"),
               filename=paste0("c_elegans.PRJNA13758.WS",WS,".canonical_geneset.gtf.gz"))
 
   } else if(WS <= 252){
@@ -80,27 +100,31 @@ get_filename_txdb <- function(WS){
 #' of the genome FASTA file for a given Wormbase release.
 #'
 #' @param WS Wormbase release number.
+#' @param protocol use https or ftp
 #'
 #' @return a vector of length 2 giving the FTP path and the filename.
 #'
-get_filename_genome <- function(WS){
+get_filename_genome <- function(WS, protocol = "https"){
+
+  base_url <- get_url_root(protocol)
+
   if(WS >= 237){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/PRJNA13758/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/PRJNA13758/"),
               filename=paste0("c_elegans.PRJNA13758.WS",WS,".genomic.fa.gz"))
 
   } else if(WS >= 197){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/"),
               filename=paste0("c_elegans.WS",WS,".genomic.fa.gz"))
 
   } else if(WS %in% seq(100, 190, 10)){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/"),
               filename=paste0("c_elegans.WS",WS,".genomic.fa.gz"))
 
   } else if(WS == 77){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS077/species/c_elegans/"),
+    path <- c(path=paste0(base_url, "WS077/species/c_elegans/"),
               filename=paste0("c_elegans.WS077.genomic.fa.gz"))
 
   } else if(WS < 197){
@@ -117,37 +141,40 @@ get_filename_genome <- function(WS){
 #' of the transcriptome FASTA file for a given Wormbase release.
 #'
 #' @param WS Wormbase release number.
+#' @param protocol use https or ftp
 #'
 #' @return a vector of length 2 giving the FTP path and the filename. Note the filename changes between versions!
 #'
-get_filename_transcriptome <- function(WS){
+get_filename_transcriptome <- function(WS, protocol = "https"){
+
+  base_url <- get_url_root(protocol)
 
   if(WS >= 244){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/PRJNA13758/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/PRJNA13758/"),
               filename=paste0("c_elegans.PRJNA13758.WS",WS,".mRNA_transcripts.fa.gz"))
 
   } else if(WS >= 237){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/PRJNA13758/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/PRJNA13758/"),
               filename=paste0("c_elegans.WS",WS,".coding_transcripts.fa.gz"))
 
   }  else if(WS == 236){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/"),
               filename=paste0("c_elegans.WS",WS,".coding_transcripts.fa.gz"))
 
   } else if(WS >= 197){
 
     warning("Full transcripts not available before WS236, downloading CDS transcripts instead (no UTR).")
 
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/"),
               filename=paste0("c_elegans.WS",WS,".cds_transcripts.fa.gz"))
 
   } else if(WS %in% seq(100, 190, 10)){
-    path <- c(path=paste0("ftp://ftp.wormbase.org/pub/wormbase/releases/WS",
-                          WS,"/species/c_elegans/"),
+    path <- c(path=paste0(base_url, "WS", WS,
+                          "/species/c_elegans/"),
               filename=paste0("c_elegans.WS",WS,".cds_transcripts.fa.gz"))
 
   } else if(WS < 197){
